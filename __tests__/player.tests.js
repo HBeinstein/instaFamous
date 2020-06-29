70cba50cddfbe2e010dbab6ce9f38fab025cf311
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-29 10:56:53 -0700

    add failing test for instantiating new Player object
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

---
+++ __tests__/player.tests.js
@@ -0,0 +1,23 @@
+import {Player} from '../src/player.js'
+
+describe('Player', ()=> {
+  jest.useFakeTimers();
+  let player;
+
+  beforeEach(function() {
+    player = new Player();
+  });
+
+  afterEach(function () {
+    // jest.clearAllTimers();
+  });
+
+  test('should have a money and fame value when it is created', ()=> {
+    expect(player.money).toEqual(200);
+    expect(player.fame).toEqual(0);
+  });
+
+});
+
+
+
